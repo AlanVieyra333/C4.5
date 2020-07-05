9a19a871bf1d9f33a62ba4e7a85e9ae0c2ef2525
Rosaura HG <rosshg.nw@gmail.com>
2020-07-05 04:06:21 -0500

    Fix bugs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import pdb
-from c45 import C45
+from c45.c45 import C45
 
-c1 = C45("../data/iris/iris.data", "../data/iris/iris.names")
+c1 = C45("./data/lenses/lenses.data", "./data/lenses/lenses.names")
 c1.fetchData()
 c1.preprocessData()
 c1.generateTree()
--- tests/test_c45.py
+++ tests/test_c45.py
@@ -3,10 +3,43 @@ import unittest
 from c45.c45 import C45
 
 class testC45Methods(unittest.TestCase):
-	c1 = C45("../data/iris/iris.data", "../data/iris/iris.names")
+	def testIris(self):
+		c1 = C45("./data/iris/iris.data", "./data/iris/iris.names")
+		c1.fetchData()
+		c1.preprocessData()
+		c1.generateTree()
 
-	def testFoo(self):
-		self.assertEqual(True, True)
+		self.__testTree(c1)
+	
+	def testLenses(self):
+		c1 = C45("./data/lenses/lenses.data", "./data/lenses/lenses.names")
+		c1.fetchData()
+		c1.preprocessData()
+		c1.generateTree()
+
+		self.__testTree(c1)
+
+	def testDivorce(self):
+		c1 = C45("./data/divorce/divorce.data", "./data/divorce/divorce.names")
+		c1.fetchData()
+		c1.preprocessData()
+		c1.generateTree()
+
+		self.__testTree(c1)
+
+	def __testTree(self, tree):
+		success_predict = 0
+		for data_row in tree.data:
+			prediction = tree.predict(data_row)
+			# print(prediction)
+			# print(data_row[-1])
+			if prediction == data_row[-1]:
+				success_predict += 1
+
+		if success_predict == len(tree.data):
+			self.assertEqual(True, True, msg ="Accuracy: " + str(success_predict) + "/" + str(len(tree.data)))
+		else:
+			self.assertEqual(False, True, msg="Accuracy: " + str(success_predict) + "/" + str(len(tree.data)))
 
 
 def main():
