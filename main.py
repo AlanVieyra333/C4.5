3742caba54051e9cc966326cfa4bc46dc19c6bdc
Alan Vieyra <alan.vieyra376@gmail.com>
2020-07-07 12:54:29 -0500

    Update test divorce

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pdb
 from c45.c45 import C45
 
-c1 = C45("./data/lenses/lenses.data", "./data/lenses/lenses.names")
+c1 = C45("./data/divorce/divorce.data", "./data/divorce/divorce.names")
 c1.fetchData()
 c1.preprocessData()
 c1.generateTree()
